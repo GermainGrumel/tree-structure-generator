a03d6b8a8da33735d99802c83614f775a502f6e1
Lahaxe Hugo <lahaxeh@gmail.com>
2020-10-01 17:38:38 +0200

    adding new breakpoint

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,22 +12,40 @@ function generateSvg(width) {
 		let parent = child[i].parentNode.parentNode.parentNode.querySelector('div');
 		let path = document.createElementNS(svgNs, 'path');
 
-		if(width > 800) {
+		if(width > 1000) {
 			x1 = child[i].offsetLeft+child[i].offsetWidth/2;
 			y1 = child[i].offsetTop;
 			x2 = parent.offsetLeft+parent.offsetWidth/2;
 			y2 = parent.offsetTop+parent.offsetHeight;
-			path.setAttribute('d', 
-				 ' M '+x1+','+y1
-				+' L '+x1+','+(y1-12)
-				+' L '+x2+','+(y2+12)
-				+' L '+x2+','+y2);
 			if(child[i].parentNode.nextElementSibling || child[i].parentNode.previousElementSibling) {
+				path.setAttribute('d', 
+					 ' M '+x1+','+y1
+					+' V '+(y1-12)
+					+' H '+x2
+					+' V '+y2);
 			} else{
 				path.setAttribute('d',
 					' M '+x1+','+y1
 					+' V '+y2);
 			}
+		} 
+		else if(width > 600) {
+			x1 = child[i].offsetLeft;
+			y1 = child[i].offsetTop+child[i].offsetHeight/2;
+			x2 = parent.offsetLeft+parent.offsetWidth;
+			y2 = parent.offsetTop+parent.offsetHeight/2;
+			if(child[i].parentNode.nextElementSibling || child[i].parentNode.previousElementSibling) {
+				path.setAttribute('d', 
+					 ' M '+x1+','+y1
+					+' H '+(x1-12)
+					+' V '+y2
+					+' H '+x2);
+			} else {
+				path.setAttribute('d',
+					' M '+x1+','+y1
+					+' H '+x2);
+			}
+
 		} else {
 			x1 = child[i].offsetLeft;
 			y1 = child[i].offsetTop+child[i].offsetHeight/2;
@@ -35,8 +53,8 @@ function generateSvg(width) {
 			y2 = parent.offsetTop+parent.offsetHeight;
 			path.setAttribute('d', 
 				 ' M '+x1+','+y1
-				+' L '+(x1-12)+','+y1
-				+' L '+x2+','+y2);
+				+' H '+(x1-12)
+				+' V '+y2);
 		}
 		path.setAttribute('stroke', "black");
 		path.setAttribute('stroke-width', "2");
@@ -53,5 +71,5 @@ function resize() {
 	generateSvg(arbo.offsetWidth);
 }
 
-// resize();
-// window.onresize = resize;
+resize();
+window.onresize = resize;
