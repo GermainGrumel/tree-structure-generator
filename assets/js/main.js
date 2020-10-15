1b5c5d2ac442d83f49fd5d7fd90c072ad1ed96ed
Lahaxe Hugo <lahaxeh@gmail.com>
2020-10-15 13:25:16 +0200

    animation svg on load/resize

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,21 @@
-let arbo = document.querySelector('ul.tree');
-var svgNs = "http://www.w3.org/2000/svg";
-let svgBG = document.createElementNS(svgNs, 'svg');
+const arbo = document.querySelector('ul.tree');
+const svgNs = "http://www.w3.org/2000/svg";
+const child = document.querySelectorAll('ul.tree ul div');
+const svgBG = document.createElementNS(svgNs, 'svg');
+var resizeTimeOut;
+
 svgBG.setAttribute('class', 'svg-wrapper ');
 arbo.parentNode.insertBefore(svgBG, arbo);
 
-let child = document.querySelectorAll('ul.tree ul div');
 
 function generateSvg(width) {
+	svgBG.style.display = 'none';
 	for (let i = 0; i < child.length; i++) {
 		let x1, y1, x2, y2;
 		let parent = child[i].parentNode.parentNode.parentNode.querySelector('div');
 		let path = document.createElementNS(svgNs, 'path');
+		path.id = "linesvg-"+i;
+		let animate = document.createElementNS(svgNs, 'animate');
 
 		if(width > 1000) {
 			x1 = child[i].offsetLeft+child[i].offsetWidth/2;
@@ -23,13 +28,13 @@ function generateSvg(width) {
 					+' V '+(y1-12)
 					+' H '+x2
 					+' V '+y2);
-			} else{
+			} else {
 				path.setAttribute('d',
 					' M '+x1+','+y1
 					+' V '+y2);
 			}
 		} 
-		else if(width > 600) {
+		else if(width >= 600) {
 			x1 = child[i].offsetLeft;
 			y1 = child[i].offsetTop+child[i].offsetHeight/2;
 			x2 = parent.offsetLeft+parent.offsetWidth;
@@ -63,12 +68,26 @@ function generateSvg(width) {
 	}
 }
 
+function branchReload(width) {
+	svgBG.style.animation =  '';
+	setTimeout(function() {
+		svgBG.style.display = 'block';
+		if(width > 1000) {
+			svgBG.style.animation =  'bgFromTop 3s ease-in';
+		} else {
+			svgBG.style.animation =  'bgFromLeft 3s ease-in';
+		}
+	}, 0);
+}
+
 function resize() {
 	svgBG.setAttribute('view-box', '0 0 '+arbo.offsetWidth+' '+arbo.offsetHeight);
 	svgBG.setAttribute('height', arbo.offsetHeight);
 	svgBG.setAttribute('width', arbo.offsetWidth);
 	svgBG.innerHTML = '';
 	generateSvg(arbo.offsetWidth);
+	clearTimeout(resizeTimeOut);
+	resizeTimeOut = setTimeout(function() {branchReload(arbo.offsetWidth)}, 100);
 }
 
 resize();
